f269497bd85355b5ab418fdfc526dcddff15f6fc
Nuno Ricardo Guimaraes <nunoricardoguimaraes@gmail.com>
2020-06-04 11:34:56 +0100

    Update utils_map.R
    It is possible to select a country and to change from the average year temperature to the maximum year temperature

--- utils_map.R
+++ utils_map.R
@@ -17,15 +17,56 @@ clim_data<-clim_data[complete.cases(clim_data),]
 
 clim_data  %>%separate(col = dt, into = c("Year", "Month", "Day"), convert = TRUE) ->clim_data
 
-clim_data %>% 
-  select(Year,AverageTemperature,Country) %>%
-  group_by(Year,Country) %>%
-  summarise(value=mean(AverageTemperature))-> clim_dataf
+
+
+#CHANGE THESE VARIABLES TO SEE NEW MAPS
+#TYPE CAN BE "average" or "max" for average and max temperature respectively
+#Region can be world or a list of any of the countries available
+#to see the list of possible countries uncomment the next line
+
+#print(unique(clim_data$Country))
+
+TYPE="max"
+region="world"
+#examples
+#region=c("Portugal")
+#region=c("Portugal","Spain")
+if(TYPE=="average"){
+  clim_data %>% 
+    select(Year,AverageTemperature,Country) %>%
+    group_by(Year,Country) %>%
+    summarise(value=mean(AverageTemperature))-> clim_dataf
+}
+
+
+
+if(TYPE=="max"){
+  clim_data %>% 
+    select(Year,AverageTemperature,Country) %>%
+    group_by(Year,Country) %>%
+    summarise(value=max(AverageTemperature))-> clim_dataf
+}
+
+if(region!="world"){
+  clim_dataf<-clim_dataf[clim_dataf$Country %in% region,]
+ 
+  g <- list(
+    fitbounds = "locations",
+    visible = TRUE
+  )
+  }else{
+    g <- list(
+      visible=TRUE
+    )
+ 
+}
+
 
 
 
 
 clim_data_test<-clim_dataf
+
 
 max_value=max(clim_data_test$value)
 min_value=min(clim_data_test$value)
@@ -34,6 +75,16 @@ min_value=min(clim_data_test$value)
 
 
 colnames(clim_data_test)<-c("Year","Country","Temperature")
+clim_data_test$Country <- factor(clim_data_test$Country)
+
+clim_data_test<-clim_data_test[complete.cases(clim_data_test),]
+
+"""
+clim_data_test %>% mutate(decade = floor(Year/10)*10) %>% 
+  group_by(decade,Country) %>% 
+  summarize_all(mean) %>% 
+  select(-Year)
+"""
 
 fig <- clim_data_test %>%
   plot_ly(
@@ -42,20 +93,25 @@ fig <- clim_data_test %>%
     locations=~Country,
     frame = ~Year,
     type = 'choropleth',
-    showlegend = F,
+    showlegend = T,
     colorscale='bluered',
     zmax=max_value,
     zmin=min_value  
     )
 
+
+
 fig <- fig %>% layout(
-  title="Evolution of the Average Temperature (C) through the years (1743-2013)"
+  title="Evolution of the Average Temperature (C) through the years (1743-2013)",
+  geo=g
 )
 
 fig <- fig %>%
   animation_opts(
     100
   )
+
+
 
 fig
 
